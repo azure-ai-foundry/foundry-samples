cf3f5c19fdcc21b183d528bc159a7fff96ee48e5
Aaron Hill <aahi@microsoft.com>
2026-02-24 11:23:42 -0800

    minor update

--- samples/REST/quickstart/quickstart-chat-with-agent.sh
+++ samples/REST/quickstart/quickstart-chat-with-agent.sh
@@ -1,3 +1,13 @@
+# Generate a response using the agent
+
+curl -X POST https://YOUR-FOUNDRY-RESOURCE-NAME.services.ai.azure.com/api/projects/YOUR-PROJECT-NAME/openai/v1/responses \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AZURE_AI_AUTH_TOKEN" \
+  -d '{
+    "agent_reference": {"type": "agent_reference", "name": "<AGENT_NAME>"},
+    "input": [{"role": "user", "content": "What is the size of France in square miles?"}]
+  }'
+
 # Optional Step: Create a conversation to use with the agent
 curl -X POST https://YOUR-FOUNDRY-RESOURCE-NAME.services.ai.azure.com/api/projects/YOUR-PROJECT-NAME/openai/v1/conversations \
   -H "Content-Type: application/json" \
@@ -15,16 +25,6 @@ curl -X POST https://YOUR-FOUNDRY-RESOURCE-NAME.services.ai.azure.com/api/projec
         ]
       }
     ]
-  }'
-
-# Generate a response using the agent
-
-curl -X POST https://YOUR-FOUNDRY-RESOURCE-NAME.services.ai.azure.com/api/projects/YOUR-PROJECT-NAME/openai/v1/responses \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $AZURE_AI_AUTH_TOKEN" \
-  -d '{
-    "agent_reference": {"type": "agent_reference", "name": "<AGENT_NAME>"},
-    "input": [{"role": "user", "content": "What is the size of France in square miles?"}]
   }'
 
 # Lets say Conversation ID created is conv_123456789. Use this in the next step
