06a0f0367ec3ec3949314ae5e10430ea48f89e01
Aaron Hill <aahi@microsoft.com>
2026-03-06 13:19:58 -0800

    removing prefix

--- samples/csharp/quickstart/create-agent/quickstart-create-agent.cs
+++ samples/csharp/quickstart/create-agent/quickstart-create-agent.cs
@@ -3,12 +3,12 @@ using Azure.AI.Projects;
 using Azure.AI.Projects.OpenAI;
 
 // Format: "https://resource_name.ai.azure.com/api/projects/project_name"
-var foundryProjectEndpoint = "your_project_endpoint";
-var foundryAgentName = "your_agent_name";
+var ProjectEndpoint = "your_project_endpoint";
+var AgentName = "your_agent_name";
 
 // Create project client to call Foundry API
 AIProjectClient projectClient = new(
-    endpoint: new Uri(foundryProjectEndpoint),
+    endpoint: new Uri(ProjectEndpoint),
     tokenProvider: new DefaultAzureCredential());
 
 // Create an agent with a model and instructions
@@ -18,6 +18,6 @@ AgentDefinition agentDefinition = new PromptAgentDefinition("gpt-5-mini") // sup
 };
 
 AgentVersion agent = projectClient.Agents.CreateAgentVersion(
-    foundryAgentName,
+    AgentName,
     options: new(agentDefinition));
 Console.WriteLine($"Agent created (id: {agent.Id}, name: {agent.Name}, version: {agent.Version})");
